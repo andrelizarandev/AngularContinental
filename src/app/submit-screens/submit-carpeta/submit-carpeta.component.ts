8e0c06f5cb2264cd66a17b0c0c3dfd52b2b86e0b
André Lizarán <113467726+andrelizarandev@users.noreply.github.com>
2024-01-28 18:11:29 -0600

    FEAT: Formularios de creación de carpetas

--- src/app/submit-screens/submit-carpeta/submit-carpeta.component.ts
+++ src/app/submit-screens/submit-carpeta/submit-carpeta.component.ts
@@ -5,6 +5,8 @@ import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Components
 import { NavigationContainerComponent } from '../../components/navigation-container/navigation-container.component';
 
 @Component({
@@ -25,17 +27,55 @@ export class SubmitCarpetaComponent {
 
   registerCarpetaForm: FormGroup;
 
-  modalidadOptions:any[] = [];
+  modalidadOptions:OptionData[] = [
+    { id:1, label:'Presencial' },
+    { id:2, label:'Semipresencial' },
+    { id:3, label:'A Distacia' },
+    { id:4, label:'Presencial y a Distancia' },
+    { id:5, label:'Semipresencial y a Distancia' },
+    { id:6, label:'Presencial y Semipresencial' },
+    { id:7, label:'Presencial, Semipresencial y a Distancia' },
+  ];
+
+  semipresencialOptions:OptionData[] = [
+    { id:1, label:'Presencial' },
+    { id:2, label:'Virtual' },
+    { id:3, label:'Blended' },
+  ];
+
+  aDistanciaOptions:OptionData[] = [
+    { id:1, label:'Presencial' },
+    { id:2, label:'Virtual' },
+    { id:3, label:'Blended' },
+  ]
+
+  presencialOptions:OptionData[] = [
+    { id:1, label:'Presencial' },
+    { id:2, label:'Virtual' },
+    { id:3, label:'Blended' },
+  ];
 
   constructor (private fb:FormBuilder) {
       
     this.registerCarpetaForm = this.fb.group({
-      modalidad: ['', Validators.required],
-      formato_presencial: ['', Validators.required],
-      formato_semipresencial: ['', Validators.required],
-      form_distancia: ['', Validators.required],
+      modalidad: [1, Validators.required],
+      formato_presencial: [1, Validators.required],
+      formato_semipresencial: [1, Validators.required],
+      form_distancia: [1, Validators.required],
     });
+
+    this.onSubmit();
 
   }
 
+  onSubmit () {
+
+  }
+
+
+}
+
+export type OptionData = {
+  id:number;
+  label:string;
 }
