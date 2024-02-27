14fe5cee16bdce14c3a6093af11041b83022d4e9
André Lizarán <113467726+andrelizarandev@users.noreply.github.com>
2024-02-26 18:31:56 -0600

    feat: nombre de opciones

--- src/app/data/data.options.ts
+++ src/app/data/data.options.ts
@@ -2,7 +2,7 @@
 import { OptionDataIdNumber } from '../screens/submit/submit-solicitud-diseno-screen/submit-solicitud-diseno-curso.component';
 
 export const ratingOptions:OptionDataIdNumber[] = [
-  { id: 1, label:'0.0' },
-  { id: 2, label:'0.5' },
-  { id: 3, label:'1.0' },
+  { id: 1, label:'No' },
+  { id: 2, label:'Ep' },
+  { id: 3, label:'Sí' },
 ];
