cfab156f1801e5b2e7165cfc938aa1693fd8b28e
André Lizarán <113467726+andrelizarandev@users.noreply.github.com>
2024-03-04 19:46:39 -0600

    feat: botón emula la carga de archivos

--- src/app/dialogs/submit/register-produccion-general-by-row/register-produccion-general-by-row.component.ts
+++ src/app/dialogs/submit/register-produccion-general-by-row/register-produccion-general-by-row.component.ts
@@ -4,7 +4,7 @@ import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-register-produccion-general-by-row',
@@ -23,19 +23,26 @@ export class RegisterProduccionGeneralByRowComponent {
   // Props
   @Input() isDialogOpen = false;
   @Output() closeDialogEmitter = new EventEmitter();
+  @ViewChild('fileInput') fileInput:ElementRef | null = null;
 
   // Vars
   currentAsignatura:null | string = null;
   currentModalidad:null | string = null;
   currentFormato:null | string = null;
   currentUnidad:null | string = null;
-
+  currentFile: File | null = null;
+  
   // Breadcrumbs
   breadcrumbItems:MenuItem[] = [];
 
   // Close
-  closeDialog = () => this.closeDialogEmitter.emit();
+  closeDialog () {
+    this.currentFile = null;
+    this.cleanTilAsignatura();
+    this.closeDialogEmitter.emit();
+  }
 
+  // Set
   setCurrentAsignatura (asignatura:string) {
     this.currentAsignatura = asignatura;
     const newItem = [{ label:asignatura, command:() => this.cleanTilAsignatura() }];
@@ -60,6 +67,7 @@ export class RegisterProduccionGeneralByRowComponent {
     this.breadcrumbItems = [...this.breadcrumbItems, ...newItem];
   }
 
+  // Clean
   cleanTilAsignatura () {
     this.currentAsignatura = null;
     this.currentModalidad = null;
@@ -84,6 +92,18 @@ export class RegisterProduccionGeneralByRowComponent {
   cleanTilUnidad () {
     this.currentUnidad = null;
     this.breadcrumbItems = this.breadcrumbItems.slice(0, 4);
+  }
+
+  clearCurrentFile () {
+    this.currentFile = null;
+  }
+
+  // On Change
+  onFileChange (event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file = (target.files as FileList)[0];
+    this.currentFile = file;
+    this.fileInput!!.nativeElement.value = '';
   }
 
 }
